8dcfe761654e8f01e544cb583910f39da1bb3efe
irfan2401 <irfanmsd2401@gmail.com>
2026-01-22 18:13:46 +0530

    my testng commit

---
+++ src/test/java/TestNGListeners/Listeners.java
@@ -0,0 +1,44 @@
+package TestNGListeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+
+    //For post actions we use listeners
+//for example if i am running a class in that class multiple test methods are there.
+// from that some test methods may fail or some will pass or some will be skipped.
+// for each conditions pass or fail or skip i want to perform some further actions
+// these are known as post actions.
+
+    //>for that i need to create:
+    //>>create a test case
+    //>>create listeners class
+    //>>create xml file and include both test cases and listeners class.
+
+    public void onStart(ITestContext context){
+        System.out.println("Test execution is started...");
+    }
+
+    public void onTestStart(ITestResult result){
+        System.out.println("Test started...");
+    }
+
+    public void onTestSuccess(ITestResult result){
+        System.out.println("Test passed...");
+    }
+
+    public void onTestFailure(ITestResult result){
+        System.out.println("Test failed...");
+    }
+
+    public void onTestSkipped(ITestResult result){
+        System.out.println("Test skipped...");
+    }
+
+    public void onFinish(ITestContext context){
+        System.out.println("Test execution is finished...");
+    }
+
+}
---
+++ src/test/java/TestNGListeners/orangehrmtestcases.java
@@ -0,0 +1,40 @@
+package TestNGListeners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class orangehrmtestcases {
+    WebDriver driver;
+
+    @Test(priority = 1)
+    void openapp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+
+    }
+
+    @Test(priority = 2)
+    void testlogo(){
+        boolean status = driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed();
+        System.out.println("Image displayed: "+status);
+    }
+
+    @Test(priority = 3)
+    void testlogin(){
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+    }
+
+    @Test(priority = 4)
+    void testclose(){
+        driver.quit();
+    }
+}
+
